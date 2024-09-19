5f59de7379a3482a6e0ad642150ab6d8c359e709
Anine <679310@stud.hvl.no>
2024-09-19 14:53:21 +0200

    Fiksa på O3

--- src/no/hvl/dat100/O3.java
+++ src/no/hvl/dat100/O3.java
@@ -1,28 +1,28 @@
 package no.hvl.dat100;
 
+import static java.lang.Integer.*;
+import static javax.swing.JOptionPane.*;
+
 /*
  * Lag et program som leser inn et heltall n > 0, beregner verdien n! (n fakultet) og skriver verdien til n! ut på skjermen, der n! = 1*2*3* … *(n-1)*n.
  */
+
 public class O3 {
 
 	public static void main(String[] args) {
-		int n = 3;
-		int b = 1;
-		int sluttn;
-		int tal;
+		int sum = 1;
+		
+		String nTxt = showInputDialog("Gi eit heiltal:");
 
-		sluttn = (n - 1) * n;
-		// b = b * (b+1);
-		for (int j = 1; j <= sluttn; j++) {
-			for (int i = 1; i <= sluttn; i++) {
-				int multiple = i * j;
-				System.out.println(multiple);
-			}
+		int n = parseInt(nTxt);
+		
+
+		for (int i = n; i >= 1; i--) {
+			sum = sum * i;
 		}
-
-		/*
-		 * do { tal = b *2 ; System.out.println(tal); b++; } while (b <= sluttn);
-		 */
+		
+		String txt = "Den totale summen er: " + sum;
+		showMessageDialog(null, txt);
 	}
 
 }
